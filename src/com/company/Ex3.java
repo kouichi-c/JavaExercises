191644480cad647fe8c01d0cc49609dfbef49ea8
hinako <hinako@192.168.3.2>
2020-12-09 16:32:39 +0900

    解答記入

--- src/com/company/Ex3.java
+++ src/com/company/Ex3.java
@@ -3,17 +3,25 @@ package com.company;
 import java.util.Scanner;
 
 public class Ex3 {
-    public static void main(String[] args) {
-        /*
-        問. 任意の3桁の整数(100 ～ 999)を標準入力から受け取り、
-        100の位、10の位、1の位の数字を足した数を標準出力してください
-        例. 入力: 476
-            出力: 17
-         */
-//    	テスト
-        System.out.println("3桁の整数を入力してください");
-        // Let's coding!
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		/*
+		問. 任意の3桁の整数(100 ～ 999)を標準入力から受け取り、
+		100の位、10の位、1の位の数字を足した数を標準出力してください
+		例. 入力: 476
+		    出力: 17
+		 */
+		System.out.println("3桁の整数を入力してください");
+		// Let's coding!
+		Scanner scanner = new Scanner(System.in);
+		int nums = scanner.nextInt();
 
-    }
+		int hundredsDigit = nums / 100;
+		int tensDigit = (nums / 10) % 10;
+		int onesDigit = nums % 10;
+
+		System.out.println(hundredsDigit + tensDigit + onesDigit);
+
+		scanner.close();
+
+	}
 }
