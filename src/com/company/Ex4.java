f13eb2b3428b14a565f73bf23c6ce58e9ba1c5a4
hinako <hinako@192.168.3.2>
2020-12-17 11:04:04 +0900

    解答記入

--- src/com/company/Ex4.java
+++ src/com/company/Ex4.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Ex4 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         /*
         問. ３つの整数を標準入力で受け取り、"どれか一つ" が0より大きかったらtrue
             どれも0より小さければfalse となるようなプログラムを書いてください。
@@ -17,6 +17,44 @@ public class Ex4 {
         Scanner scanner = new Scanner(System.in);
 
         // Let's coding!!
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        scanner.close();
+
+        //falseになる条件を考える
+        boolean result = true;
+        if(num1 > 0 && num2 >0) {
+        	result = false;
+        }else if(num1 > 0 && num3 > 0) {
+        	result = false;
+        }else if(num2 > 0 && num3 > 0) {
+        	result = false;
+        }else if(num1 > 0 && num2 > 0 && num3 >0) {
+        	result = false;
+        }else if(num1 < 0 && num2 < 0 && num3 <0) {
+        	result = false;
+        }else {
+        	result = true;
+        }
+
+        //trueになる条件を考える
+        boolean result2 = false;
+        if(num1 > 0 && num2 <= 0 && num3 <= 0) {
+        	result2 = true;
+        }else if(num1 <= 0 && num2 > 0 && num3 <= 0) {
+        	result2 = true;
+        }else if(num1 <= 0 && num2 <= 0 && num3 > 0) {
+        	result2 = true;
+        }else {
+        	result2 = false;
+        }
+
+
+        System.out.println(result);
+        System.out.println(result2);
+
 
     }
 }
